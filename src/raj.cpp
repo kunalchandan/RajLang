7e73455b00a461a1147495b71e57e34b3edf8f80
Kunal Chandan <kunal_chandan@outlook.com>
2023-07-29 07:52:48 -0400

    Add CLI parsing

--- src/raj.cpp
+++ src/raj.cpp
@@ -11,15 +11,20 @@
 
 #include "Lexer.hpp"
 
-int main() {
-    CLI::App app{"Raj Language Compiler"};
+int main(int argc, char** argv) {
+    CLI::App    app{"Raj Language Compiler"};
+    std::string file;
+    // TODO:: Pass file into the source_files vector
+    app.add_option("-f,--file", file, "Require a source file")->required()->check(CLI::ExistingFile);
+    CLI11_PARSE(app, argc, argv);
 
     std::vector<std::filesystem::path> source_files = {"examples/declaration_array.raj"};
+    std::vector<SourceCode>            raw_file     = read_raw_file(source_files);
 
-    std::vector<SourceCode> raw_file = read_raw_file(source_files);
     for(const auto& file : raw_file) {
         std::vector<Lexeme> lexemes = lex_file(file);
 
+        // Basic debugging
         std::clog << std::endl << "Lexemes Identified: " << std::endl;
         for(const auto& lex : lexemes) {
             std::clog << lex.tokens << " " << magic_enum::enum_name(lex.lexeme_type) << std::endl;
