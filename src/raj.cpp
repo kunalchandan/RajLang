e1997b592c9203b5c7d4b13cd97e22f820b59030
Kunal Chandan <kunal_chandan@outlook.com>
2023-08-05 03:45:27 -0400

    Update main

--- src/raj.cpp
+++ src/raj.cpp
@@ -1,15 +1,19 @@
+#include <algorithm>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <string>
-
-#include <cctype>
 
 #include <CLI/CLI.hpp>
 #include <catch.hpp>
 #include <magic_enum_all.hpp>
+#include <magic_enum.hpp>
 
+#include "AST.hpp"
 #include "Lexer.hpp"
+
+#include "logging.hpp"
+
+extern LogLevel currentLogLevel;
 
 int main(int argc, char** argv) {
     CLI::App    app{"Raj Language Compiler"};
@@ -18,17 +22,23 @@ int main(int argc, char** argv) {
     app.add_option("-f,--file", file, "Require a source file")->required()->check(CLI::ExistingFile);
     CLI11_PARSE(app, argc, argv);
 
-    std::vector<std::filesystem::path> source_files = {"examples/declaration_array.raj"};
+    currentLogLevel                                 = LogLevel::ERROR;
+    std::vector<std::filesystem::path> source_files = {"examples/function_calling.raj"};
     std::vector<SourceCode>            raw_file     = read_raw_file(source_files);
 
     for(const auto& file : raw_file) {
-        std::vector<Lexeme> lexemes = lex_file(file);
-
+        currentLogLevel                                   = LogLevel::ERROR;
+        std::vector<std::tuple<Lexeme, Location>> lexemes = lex_file(file);
+        lexemes                                           = filter_spaces(lexemes);
         // Basic debugging
-        std::clog << std::endl << "Lexemes Identified: " << std::endl;
+        LOG_DEBUG("Lexemes Identified: ")
         for(const auto& lex : lexemes) {
-            std::clog << lex.tokens << " " << magic_enum::enum_name(lex.lexeme_type) << std::endl;
+            LOG_DEBUG(std::get<0>(lex).tokens + " " +
+                      std::string(magic_enum::enum_name(std::get<0>(lex).lexeme_type)))
         }
+        // Generate the AST for the lexemes
+        currentLogLevel = LogLevel::DEBUG;
+        generate_ast(lexemes);
     }
 
     return 0;
